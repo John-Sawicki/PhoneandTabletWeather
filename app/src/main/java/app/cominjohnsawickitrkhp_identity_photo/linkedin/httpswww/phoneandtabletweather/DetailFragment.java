f02bb5af3e40fff137ce889b9404e0f3a3a3b0dd
john-sawicki <3Gxapnil>
2017-12-10 17:05:24 -0600

    added location frag

--- app/src/main/java/app/cominjohnsawickitrkhp_identity_photo/linkedin/httpswww/phoneandtabletweather/DetailFragment.java
+++ app/src/main/java/app/cominjohnsawickitrkhp_identity_photo/linkedin/httpswww/phoneandtabletweather/DetailFragment.java
@@ -1,4 +1,31 @@
 package app.cominjohnsawickitrkhp_identity_photo.linkedin.httpswww.phoneandtabletweather;
 
-public class DetailFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailFragment extends Fragment{
+    TextView maxTemp, minTemp, main;
+    ImageView iconImageView;
+    String[] formattedString = new String[11];
+    String imageIcon;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.detail_fragment, container, false);
+        maxTemp = (TextView) view.findViewById(R.id.max_temp);
+        minTemp = (TextView) view.findViewById(R.id.min_temp);
+        main = (TextView) view.findViewById(R.id.main_description);
+        iconImageView = (ImageView) view.findViewById(R.id.icon);
+        return view;
+    }
+
+    public DetailFragment() {
+    }
 }
---
+++ app/src/main/java/app/cominjohnsawickitrkhp_identity_photo/linkedin/httpswww/phoneandtabletweather/LocationFragment.java
@@ -0,0 +1,45 @@
+package app.cominjohnsawickitrkhp_identity_photo.linkedin.httpswww.phoneandtabletweather;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocationFragment extends Fragment {
+    TextView time, location;
+    String userTime="";
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.location_fragment,container, false);
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss MM-dd-yyyy");
+        time = (TextView)view.findViewById(R.id.time_textView);
+        location = (TextView)view.findViewById(R.id.location_textView);
+        userTime = dateFormat.format(date);
+        time.setText(userTime);
+        return view;
+    }
+    public void updateLocation(String[] formattedString){
+        location.setText(formattedString[0]);
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString("userTime",userTime );
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onViewStateRestored(Bundle savedInstanceState) {
+        time.setText(savedInstanceState.getString("userTime"));
+        super.onViewStateRestored(savedInstanceState);
+    }
+
+}
